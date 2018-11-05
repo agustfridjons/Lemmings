8a58d297373faa33a62a24ad58952ad7d9cd29c6
Flow-flex-the-complex <fth29@hi.is>
2018-11-05 17:52:01 +0000

    Grid

--- grid.js
+++ grid.js
@@ -12,7 +12,7 @@ Grid.prototype.createGrid = function(width,height){
     var blocks = []
     for(var i = this.halfHeight; i < height; i+=this.halfHeight*2){
         for(var j = this.halfWidth; j < width; j+=this.halfWidth*2){
-            blocks.push([i,j]);
+            blocks.push([i,j,false]);
         }
     }
     return blocks;
@@ -20,9 +20,13 @@ Grid.prototype.createGrid = function(width,height){
 
 Grid.prototype.render = function(ctx,blocks){
     ctx.strokeStyle = "black";
+    ctx.fillStyle = "black";
     for(var i = 0; i < blocks.length; i++){
         var Boi = blocks[i];
-        ctx.strokeRect(Boi[0]-20,Boi[1]-20,this.halfWidth*2,this.halfHeight*2);
+        if(Boi[2]){
+            ctx.fillRect(Boi[0]-20,Boi[1]-20,this.halfWidth*2,this.halfHeight*2);
+        } else {
+        }
     }
 };
 
@@ -30,11 +34,11 @@ Grid.prototype.render = function(ctx,blocks){
 Grid.prototype.findNearestBlock = function(xPos, yPos){
     for(var i = 0; i < blocks.length; i++){
         var Boi = blocks[i];
-        if(Boi[0] + this.halfWidth < xPos 
-            && Boi[0] - this.halfWidth > xPos
-            && Boi[1] + this.halfHeight < yPos
-            && Boi[1] - this.halfHeight > yPos){
-                return blocks[i];
+        if(Boi[0] + this.halfWidth > xPos 
+            && Boi[0] - this.halfWidth < xPos
+            && Boi[1] + this.halfHeight > yPos
+            && Boi[1] - this.halfHeight < yPos){
+                return i;
         }
     }
 };
